52738da0aa8b38d0e00e9a0877a1c57d1773c1ef
salaboy <salaboy@gmail.com>
2020-09-11 08:31:38 +0100

    adding list triggers

--- src/main/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterController.java
+++ src/main/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterController.java
@@ -11,6 +11,8 @@ import io.cloudevents.core.provider.EventFormatProvider;
 import io.cloudevents.jackson.JsonFormat;
 import io.fabric8.knative.client.DefaultKnativeClient;
 import io.fabric8.knative.client.KnativeClient;
+import io.fabric8.knative.eventing.v1alpha1.Trigger;
+import io.fabric8.knative.eventing.v1alpha1.TriggerList;
 import io.fabric8.knative.serving.v1.Service;
 import io.fabric8.knative.serving.v1.ServiceList;
 import io.zeebe.client.api.response.DeploymentEvent;
@@ -116,12 +118,12 @@ public class ZeebeCloudEventsRouterController {
 //        DeploymentEvent deploymentEvent = zeebeClient.newDeployCommand().addResourceStringUtf8(dwp.getName(), dwp.getWorkflowDefinition()).send().join();
         try (KnativeClient kn = new DefaultKnativeClient()) {
             // Get all Service objects
-            ServiceList services = kn.services()
+            TriggerList triggerList = kn.triggers()
                     .inNamespace("default")
                     .list();
             // Iterate through list and print names
-            for (Service svc : services.getItems()) {
-                System.out.println(svc.getMetadata().getName());
+            for (Trigger trigger : triggerList.getItems()) {
+                System.out.println(trigger.getMetadata().getName()  + " -> Broker: " + trigger.getSpec().getBroker()  + " -> Filter attr type: " + trigger.getSpec().getFilter().getAttributes().get("type") + " -> Subscriber: " + trigger.getSpec().getSubscriber().getUri());
             }
         }
 
