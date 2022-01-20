c4af88435b9b2c791871fdb8f3320e62420fec07
Lionel Untereiner <lionel.untereiner@geosiris.com>
2022-01-20 10:20:13 +0100

    Update zeebe to 1.3.0

--- src/main/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterApp.java
+++ src/main/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterApp.java
@@ -1,6 +1,6 @@
 package io.zeebe.cloud.events.router;
 
-import io.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
