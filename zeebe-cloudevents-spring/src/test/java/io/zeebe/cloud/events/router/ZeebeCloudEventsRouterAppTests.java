b9011192e23271576cc166fe16d8c36f72e2ad0f
Lionel Untereiner <lionel.untereiner@geosiris.com>
2022-01-24 16:03:05 +0100

    fix: dependencies + do not run fmt and convergence for now

--- zeebe-cloudevents-spring/src/test/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterAppTests.java
+++ zeebe-cloudevents-spring/src/test/java/io/zeebe/cloud/events/router/ZeebeCloudEventsRouterAppTests.java
@@ -1,11 +1,11 @@
 package io.zeebe.cloud.events.router;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZeebeCloudEventsRouterAppTests {
 
